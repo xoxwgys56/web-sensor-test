62bea7e19af772584c75021aeea035db8d875d93
dwkim <lololtoday@gmail.com>
2020-12-20 22:06:23 +0900

    chore: add request permission
    about device motion

--- index.js
+++ index.js
@@ -16,4 +16,29 @@ setInterval(function() {
 }, 1000)
 
 const test_target = document.querySelector('#test-target');
-test_target.innerHTML = String(window.DeviceMotionEvent.acceleration);
+test_target.innerHTML = String(window.DeviceMotionEvent);
+
+
+if ( location.protocol != "https:" ) {
+    location.href = "https:" + window.location.href.substring( window.location.protocol.length );
+    }
+    
+    function permission () {
+        if ( typeof( DeviceMotionEvent ) !== "undefined" && typeof( DeviceMotionEvent.requestPermission ) === "function" ) {
+            // (optional) Do something before API request prompt.
+            DeviceMotionEvent.requestPermission()
+                .then( response => {
+                // (optional) Do something after API prompt dismissed.
+                if ( response == "granted" ) {
+                    window.addEventListener( "devicemotion", (e) => {
+                        // do something for 'e' here.
+                    })
+                }
+            })
+                .catch( console.error )
+        } else {
+            alert( "DeviceMotionEvent is not defined" );
+        }
+    }
+    const btn = document.getElementById( "request" );
+    btn.addEventListener( "click", permission );
