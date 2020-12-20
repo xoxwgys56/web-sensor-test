9e7fd90093a19b650d6c6e9d3472ec2cdc42f7ca
dwkim <lololtoday@gmail.com>
2020-12-20 21:54:30 +0900

    chore: edit coe to measure acceleration
    hmm why not work it?

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const time_offset = 0.01;
 // total collected data count
 const max_count = 1500;
 
-DeviceMotionEvent.acceleration;
-
 const z_value_dom = document.querySelector('#z-value');
 const interval_dom = document.querySelector('#interval');
 
 window.addEventListener('devicemotion', event => {
     z_value_dom.innerHTML = event.acceleration.z;
-    interval_dom.innerHTML = event.interval
-});
+    interval_dom.innerHTML = event.interval;
+}, true);
+
+setInterval(function() {
+    console.log(window.DeviceMotionEvent.acceleration)
+}, 1000)
+
+const test_target = document.querySelector('#test-target');
+test_target.innerHTML = String(window.DeviceMotionEvent.acceleration);
