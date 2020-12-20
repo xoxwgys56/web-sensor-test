efd9d9544e8cd700ece7446675ade2b99b9fc196
dwkim <lololtoday@gmail.com>
2020-12-20 22:15:03 +0900

    chore: comment codes..
    which not used

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const max_count = 1500;
 const z_value_dom = document.querySelector('#z-value');
 const interval_dom = document.querySelector('#interval');
 
-window.addEventListener('devicemotion', event => {
-    z_value_dom.innerHTML = event.acceleration.z;
-    interval_dom.innerHTML = event.interval;
-}, true);
+// window.addEventListener('devicemotion', event => {
+//     z_value_dom.innerHTML = event.acceleration.z;
+//     interval_dom.innerHTML = event.interval;
+// }, true);
 
 setInterval(function() {
     // console.log(window.DeviceMotionEvent.acceleration)
