43eed42a32994e76b270351a7b427c07acc6eb8a
dwkim <lololtoday@gmail.com>
2021-01-06 00:22:34 +0900

    chore: ask permission

--- acceleration.js
+++ acceleration.js
@@ -1,9 +1,18 @@
-let acl = new Accelerometer({frequency: 60});
+const z_value_dom = document.querySelector('#z-value');
+const interval_dom = document.querySelector('#interval');
 
-acl.addEventListener('reading', () => {
-    console.log("Acceleration along the X-axis " + acl.x);
-    console.log("Acceleration along the Y-axis " + acl.y);
-    console.log("Acceleration along the Z-axis " + acl.z);
-  });
+// let acl = new Accelerometer({frequency: 100});
+
+// acl.addEventListener('reading', () => {
+//     console.log("Acceleration along the X-axis " + acl.x);
+//     console.log("Acceleration along the Y-axis " + acl.y);
+//     console.log("Acceleration along the Z-axis " + acl.z);
+
+//     z_value_dom.innerHTML = "Acceleration along the Z-axis " + acl.z
+//   });
   
-  acl.start();
+//   acl.start();
+
+if (DeviceMotionEvent && typeof DeviceMotionEvent.requestPermission == "function") {
+    DeviceMotionEvent.requestPermission();
+}
